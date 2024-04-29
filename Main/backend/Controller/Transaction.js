cca578a39d396353574b5773a01b4997bcdb4ebb
hemant052004 <kashyaphemant2004@gmail.com>
2024-04-29 15:45:24 +0530

    new

--- Main/backend/Controller/Transaction.js
+++ Main/backend/Controller/Transaction.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import Participant from '../models/Participant.js'; // Import Participant model
+import rateLimit from "express-rate-limit";
 
+// Apply rate limiting middleware
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
 
 
 const Transactions = async (req, res) => {
