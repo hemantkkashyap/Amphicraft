5dd25ee406279a88829361553cca706151b0a7f0
hemant052004 <kashyaphemant2004@gmail.com>
2024-04-29 17:07:11 +0530

    New

--- Main/frontend/src/Components/Home/Home.js
+++ Main/frontend/src/Components/Home/Home.js
@@ -79,36 +79,37 @@ export default function Home() {
             <p>
               <span>
                 <img
-                  src="./images/one.png"
+                  src=".images/one.png"
                   alt=""
                 />
               </span>
             </p>
             <p>
               <span>
-                <img src="./images/two.png" alt="" />
+                <img src="images/two.png" alt="" />
               </span>
             </p>
             <p>
               <span>
-                <img src="./images/three.png" alt="" />
+                <img src="images/three.png" alt="" />
               </span>
             </p>
             <p>
               <span>
-                <img src="./images/Slide1.png" alt="" />
+                <img src="images/Slide1.png" alt="" />
               </span>
             </p>
             <p>
               <span>
-                <img src="./images/Slide2.png" alt="" />
+                <img src="images/Slide2.png" alt="" />
               </span>
             </p>
             <p>
               <span>
-                <img src="./images/Slide3.png" alt="" />
+                <img src="images/Slide3.png" alt="" />
               </span>
             </p>
+
           </div>
         </div>
         <div className="contanier9" data-aos="fade-up">
