b874e284368a3454519bb3ef308b0845533618a4
hemant052004 <kashyaphemant2004@gmail.com>
2024-06-28 14:46:59 +0530

    update

--- Main/frontend/src/assets/Home/Event.jsx
+++ Main/frontend/src/assets/Home/Event.jsx
@@ -44,7 +44,9 @@ export default function Event() {
 
   const fetchEvents = async () => {
     try {
-      const response = await fetch("https://amphicraft-api.vercel.app/api/auth/events");
+      const response = await fetch(
+        "https://amphicraft-api.vercel.app/api/auth/events"
+      );
       if (!response.ok) {
         throw new Error("Failed to fetch events");
       }
@@ -72,7 +74,7 @@ export default function Event() {
       <Navbar />
       <section className="w-full h-[100vh] mt-20 p-5">
         <div className="w-full h-auto p-5">
-        {categories.map((category) => (
+          {categories.map((category) => (
             <button
               key={category}
               className={`border border-black p-2 rounded-lg m-2 ${
@@ -86,28 +88,26 @@ export default function Event() {
         </div>
 
         <div className="flex flex-wrap gap-10 justify-center w-full h-auto p-5">
-  {events.map((event, index) => (
-            <Link
-              to={`/register`}
-              state={{ event }}
-              key={index}
-            >
+          {events.map((event, index) => (
+            <Link to={`/register`} state={{ event }} key={index}>
               <div
-                className="flex flex-col items-center justify-center w-[300px] h-[300px] bg-white rounded-lg text-white" 
+                className="flex flex-col items-center justify-center w-[300px] h-[300px] bg-white rounded-lg text-white"
                 style={{
-                  backgroundColor:categoryColors[event.category],
+                  backgroundColor: categoryColors[event.category],
                 }}
-                >
-
-<div className="flex justify-center items-center text-3xl w-full h-[45%]">
-
-                  <p className="w-[100px] h-[100px] rounded-full bg-white opacity-50 flex items-center justify-center text-black">10</p>
+              >
+                <div className="flex justify-center items-center text-3xl w-full h-[45%]">
+                  <p className="w-[100px] h-[100px] rounded-full bg-white opacity-50 flex items-center justify-center text-black">
+                    10
+                  </p>
                 </div>
                 <div className="flex flex-col items-center w-full h-[55%]">
                   <div className="text-center w-full">
                     <p className="capitalize text-xl">{event.eventname}</p>
                   </div>
-                  <p className="w-full h-[40%] text-center p-3">{event.eventdetail}</p>
+                  <p className="w-full h-[40%] text-center p-3">
+                    {event.eventdetail}
+                  </p>
 
                   <button
                     className="w-[80%] h-[50px] rounded-lg"
@@ -121,7 +121,6 @@ export default function Event() {
               </div>
             </Link>
           ))}
-        
         </div>
       </section>
     </>
